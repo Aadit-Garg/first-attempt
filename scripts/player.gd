076615a222ad6085e282cc1da7bf381fba18c2f3
Aadit Garg <69139031+Aadit-Garg@users.noreply.github.com>
2026-01-03 20:35:25 +0530

    fixed light

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var point_light: PointLight2D = $flash_light
+@onready var point_light: PointLight2D = $PointLight2D
 
 
 func _physics_process(delta: float) -> void:
@@ -12,7 +12,7 @@ func _physics_process(delta: float) -> void:
 	velocity+=Vector2(delta,delta)#make movement feel same for different fps
 	#light will look here
 	point_light.look_at(mouse_pos)
-	point_light.rotation_degrees += 180
+	point_light.rotation_degrees += 360
 	var look_vec = mouse_pos - global_position
 	var angle = look_vec.angle()
 	var snapped_angle = snapped(angle, PI / 4)
