5277e48ee8fa11a58b3b89cb0809835f168e9a4e
Aadit Garg <69139031+Aadit-Garg@users.noreply.github.com>
2026-01-08 00:38:22 +0530

    feat: add initial level scenes, player scene, and kill script.a

--- scripts/kill.gd
+++ scripts/kill.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var glitch_rect: Control = get_tree().get_current_scene().get_node("canvas/CanvasLayer/ColorRect")
 
 func _on_body_entered(body: Node2D) -> void:
 	print("YOU DIED")
@@ -10,4 +11,7 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_timer_timeout() -> void:
 	Engine.time_scale=1
 	GameManager.infection_level=0
+	# Reset the infection shader
+	if glitch_rect and glitch_rect.material:
+		glitch_rect.material.set_shader_parameter("infection_level", 0)
 	get_tree().reload_current_scene()
