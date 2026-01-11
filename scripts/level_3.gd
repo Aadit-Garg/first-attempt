30db21746ba6cd7b4580bb1b872a01320740e231
PranshuNalwa <pranshunalwa0316@gmail.com>
2026-01-11 14:26:48 +0530

    some last changes

--- scripts/level_3.gd
+++ scripts/level_3.gd
@@ -3,11 +3,12 @@ extends Node2D
 @onready var death_screen: ColorRect = $CanvasLayer/death_screen
 @onready var glitch_rect: Control = $canvas/CanvasLayer/ColorRect
 @onready var transition_rect = $CanvasLayer2/TransitionRect
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameManager.infection_level=0
-	GameManager.rate=6
-	GameManager.decrease=15
+	GameManager.rate=5
+	GameManager.decrease=18
 	glitch_rect.material.set_shader_parameter("infection_level", 0)
 	death_screen.visible=false
 	transition_rect.modulate.a = 0.0
