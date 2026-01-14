920a70ba8f6bb447b305791b866dc3a5b97c8d68
PranshuNalwa <pranshunalwa0316@gmail.com>
2026-01-14 17:46:37 +0530

    passage updates

--- scripts/local_infection_level_passage_gun.gd
+++ scripts/local_infection_level_passage_gun.gd
@@ -14,3 +14,10 @@ func death():
 	glitch_rect.material.set_shader_parameter("infection_level", 0)
 	death_screen.visible = true
 	get_tree().paused = true
+
+func _on_button_pressed() -> void:
+	print("pressed")
+	GameManager.infection_level = 0
+	death_screen.visible = false
+	get_tree().paused = false
+	get_tree().reload_current_scene()
--- scripts/passage_get_gun.gd
+++ scripts/passage_get_gun.gd
@@ -7,7 +7,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	death_screen.visible=false
-
+	GameManager.gun_found=false
+	GameManager.shooting=false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
